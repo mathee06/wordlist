64a31b01def563f37e1802d0be6804f985331aeb
Mathee Sivananthan <sivanm2@mcmaster.ca>
2015-06-16 18:07:12 -0400

    Final revision

--- words.py
+++ words.py
@@ -1,16 +1,31 @@
 #! /usr/bin/env/ python
+#M. Sivananthan
+
+import string
 
 def main():
-	x = "hello world"
-	wordlist = ["apple", "con", "concave", "jig", "jigsaw", "saw", "very", "zebra"]
-	for word in wordlist:
-		partA = word
-		if partA in wordlist:
-			
-			print "true"
-		else:
-			print "false"
+	wordlist = ["apple", "con", "cave", "concave", "jig", "jigsaw", "saw", "very", "zebra"]
+	specialList = []
 
-		print word
+	for substringA in wordlist:
+		print "Checking word: " + substringA
+		for word in wordlist:
+			# skips checking the word against itself
+			if (substringA != word) :
+				print "Checking " + substringA + " against current word: " + word
+				if substringA in word:
+					substringB = word.replace(substringA, "")
+					print "substringA: " + substringA + " exists in: " + word
+					print "looking for: " + substringB
+					if substringB in wordlist:
+						print "EUREKA: WE HAVE SOMETHING!"
+						print substringA + " " + substringB + " " + word
+						specialList.append(word)
+						wordlist.remove(substringA)
+						wordlist.remove(substringB)
+						wordlist.remove(word)
+
+	print "The following list of words appear as substrings within the supplied dictionary: " + string.translate(str(specialList), None, "'")
+
 
 main()
